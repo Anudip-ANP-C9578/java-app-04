f9b6723f5f543a108ebc6b32cc52b38425d46c37
Ameeda M <ameeda071@gmail.com>
2024-10-25 09:33:54 +0530

    Multiplication of TwoNonDecimalNumbers

--- src/main/java/org/anudip/App.java
+++ src/main/java/org/anudip/App.java
@@ -1,7 +1,33 @@
+/*
+*creating a package
+*/
 package org.anudip;
+
+/* 
+*importing package and class
+*/
+import org.anudip.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
 
 public class App {
 	public static void main(String[] args) {
 		
+	/*
+    *creating object
+    */	 
+	TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+	
+	System.out.println(twoNonDecimalNumbersMultiplication.multiply(10,2)); //output:20
+	
+	System.out.println(twoNonDecimalNumbersMultiplication.multiply(10,-2)); //output:-1
+	
+	System.out.println(twoNonDecimalNumbersMultiplication.multiply(-10,2)); //output:-1
+	
+	System.out.println(twoNonDecimalNumbersMultiplication.multiply(-10,-2)); //output:-1
+	
+	System.out.println(twoNonDecimalNumbersMultiplication.multiply(10,0)); //output:-2
+	
+	System.out.println(twoNonDecimalNumbersMultiplication.multiply(0,2)); //output:-2
+	
+	System.out.println(twoNonDecimalNumbersMultiplication.multiply(0,0)); //output:-2
 	}
 }
