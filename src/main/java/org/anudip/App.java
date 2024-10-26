a16a3b892206d299120ced56e024e834f2e2ea09
Pallavin <pallavin6094@gmail.com>
2024-10-26 15:48:50 +0530

    added the Multiplication result.

--- src/main/java/org/anudip/App.java
+++ src/main/java/org/anudip/App.java
@@ -1,7 +1,23 @@
 package org.anudip;
-
+import org.anudip.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
 public class App {
-	public static void main(String[] args) {
-		
-	}
-}
+		public static void main(String[] args) {
+		TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication= new TwoNonDecimalNumbersMultiplication();
+		long result1 = twoNonDecimalNumbersMultiplication.multiply(10,2);
+		System.out.println(result1);
+		long result2 = twoNonDecimalNumbersMultiplication.multiply(10,-2);
+		System.out.println(result2);
+		long result3 = twoNonDecimalNumbersMultiplication.multiply(-10,2);
+		System.out.println(result3);
+		long result4 = twoNonDecimalNumbersMultiplication.multiply(-10,-2);
+		System.out.println(result4);
+		long result5 = twoNonDecimalNumbersMultiplication.multiply(10,0);
+		System.out.println(result5);
+		long result6 = twoNonDecimalNumbersMultiplication.multiply(0,2);
+		System.out.println(result6);
+		long result7 = twoNonDecimalNumbersMultiplication.multiply(0,0);
+		System.out.println(result7);
+		}
+}	
+	
+	
