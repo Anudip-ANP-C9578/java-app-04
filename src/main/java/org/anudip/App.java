61ab5e23a619e608c96bb8b64fb55b6446f4d74b
Abhishekshettyabhi <abhishetty1330@gmail.com>
2024-10-28 09:52:35 +0530

    Added assignment 4

--- src/main/java/org/anudip/App.java
+++ src/main/java/org/anudip/App.java
@@ -1,7 +1,33 @@
 package org.anudip;
 
-public class App {
-	public static void main(String[] args) {
-		
-	}
+import org.anudip.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
+
+public class App
+{
+public static void main(String[] args)
+{
+TwoNonDecimalNumbersMultiplication twonondecimalnumbersmultiplication = new TwoNonDecimalNumbersMultiplication();
+
+long result1 = twonondecimalnumbersmultiplication.multiply(10,2);
+System.out.println(result1);
+
+long result2 = twonondecimalnumbersmultiplication.multiply(10,-2);
+System.out.println(result2);
+
+long result3 = twonondecimalnumbersmultiplication.multiply(-10,2);
+System.out.println(result3);
+
+long result4 = twonondecimalnumbersmultiplication.multiply(-10,-2);
+System.out.println(result4);
+
+long result5 = twonondecimalnumbersmultiplication.multiply(10,0);
+System.out.println(result5);
+
+long result6 = twonondecimalnumbersmultiplication.multiply(0,2);
+System.out.println(result6);
+
+long result7 = twonondecimalnumbersmultiplication.multiply(0,0);
+System.out.println(result7);
+
+}
 }
